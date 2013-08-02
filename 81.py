f3332bc6ca3f908a7f6de6043443eb0c8da70dde
Ehtesh Choudhury <merlockmagus@gmail.com>
2013-08-02 15:20:47 -0400

    Transitioned from method based to class based for the matrix. Now I can color matrices, too!

--- 81.py
+++ 81.py
@@ -1,18 +1,27 @@
+from common import bc
 
-def read_matrix(filename):
-    f = open(filename).read()
-    matrix_str = f.strip().split("\n")
-    matrix_int = []
+class Matrix(object):
+    def __init__(self, filename):
+        f = open(filename).read()
+        matrix_str = f.strip().split("\n")
+        matrix_int = []
 
-    for row in matrix_str:
-        row_int = [int(cell) for cell in row.split(",")]
-        matrix_int.append(row_int)
+        for row in matrix_str:
+            row_int = [int(cell) for cell in row.split(",")]
+            matrix_int.append(row_int)
 
-    return matrix_int
+        self.matrix = matrix_int
+        self.rows = len(matrix_int)
+        self.columns = len(matrix_int[0])
 
-def print_matrix(matrix, selected=[]):
-    for row in matrix:
-        for cell in row:
-            print cell
+    def print_matrix(self, selected=[]):
+        for rownum in xrange(self.rows):
+            for columnnum in xrange(self.columns):
+                if (rownum,columnnum) in selected:
+                    print (bc.FAIL+"{0:4}"+bc.ENDC).format(self.matrix[rownum][columnnum]),
+                else:
+                    print "{0:4}".format(self.matrix[rownum][columnnum]),
+            print
 
-matrix = read_matrix("matrix-easy.txt")
+matrix = Matrix("matrix-easy.txt")
+matrix.print_matrix(selected=[(1,1),(2,2)])
