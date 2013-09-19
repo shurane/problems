884227aa0dd6908669e038adde351a6d15fb5c10
Ehtesh Choudhury <merlockmagus@gmail.com>
2013-09-19 16:51:33 -0400

    More complicated than I thought it was. What's an easier way to form a mathematical expression? I'm really just relying on python's eval right now.

--- 93.py
+++ 93.py
@@ -1,6 +1,7 @@
-from itertools import combinations, chain, permutations, product
-import functools
+from itertools import combinations, chain, permutations, product, count
 import operator
+import IPython
+from StringIO import StringIO
 
 ops = [operator.add, operator.sub, operator.div, operator.mul]
 
@@ -12,7 +13,7 @@ def opsmap(op):
     if op in mapping:
         return mapping[op]
     else:
-        return None
+        return "_"
 
 #from http://docs.python.org/2/library/itertools.html#recipes
 def powerset(iterable):
@@ -30,17 +31,54 @@ def generate_digits():
 # 4 operators, choose 3:    len(combinations_with_replacement(ops,3))
 # bracket placement:        len(powerset(combinations(range(len(digits)),2)))
 # total?
+# wait, brackets can't overlap. I can't have ((0,2),(1,3)) as brackets!
+#   equivalent: (a `op` (b `op2` c) `op3` d)
+#   incorrect, but it becomes a new set of brackets: ((0,3),(1,2))
+#   since I'm throwing the results in a set(), repeats don't matter
 
 def form_combinations2(digits,ops):
     digit_permutations = permutations(digits)
-    ops_combinations = product(ops,repeat=len(digits)-1)
-    bracket_combinations = powerset(combinations(range(len(digits)),2))
-    count = 0
+    ops_combinations = list(product(ops,repeat=len(digits)-1))
+    bracket_combinations = list(powerset(combinations(range(len(digits)),2)))
+    combo = 0
     for digitset in digit_permutations:
         for opset in ops_combinations:
             for bracketset in bracket_combinations:
-                print(digitset,map(opsmap,opset),bracketset)
-                count +=1
-    print count
+                flattened = []
+                for left, right in bracketset:
+                    flattened.append((left,"left"))
+                    flattened.append((right,"right"))
+                flattened = sorted(flattened)
+
+                equation = StringIO()
+                print(flattened)
+                while flattened and flattened[0][0] == 0:
+                    elem = flattened.pop(0)
+                    if elem[1] == "left":
+                        equation.write("(")
+                    else:
+                        equation.write(")")
+
+                equation.write(digitset[0])
+                for index,op,digit in zip(count(),opset,digitset[1:]):
+                    equation.write(opsmap(op))
+
+                    while flattened and flattened[0][0] == index:
+                        elem = flattened.pop(0)
+                        if elem[1] == "left":
+                            equation.write("(")
+                        else:
+                            equation.write(")")
+
+                    equation.write(digit)
+
+                if combo == 1700:
+                    IPython.embed()
+
+                equation.close()
+                #print(digitset,map(opsmap,opset),bracketset)
+                #print(combo,bracketset)
+                combo +=1
+    print combo
 
 form_combinations2([1,2,3,4],ops)
