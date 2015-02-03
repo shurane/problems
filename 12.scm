7afb6e0780a472798d2e45b84137fda02a5031e7
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-02-03 01:37:14 -0500

    speed up (simplefactor)/(factor)

--- 12.scm
+++ 12.scm
@@ -1,25 +1,25 @@
 #lang racket
 
 (require racket/generator)
+(require profile)
 
-;(define (factors number)
-  ;(for/list ([divisor (range 1 (+ 1 (inexact->exact (floor (sqrt number)))))]
-             ;#:when (eq? 0 (modulo number divisor)))
-            ;divisor))
-(define (simplefactors number)
-  (cons number 
-    (for/list ([num (range 1 (+ 1 (/ number 2)))]
-                #:when (eq? 0 (modulo number num)))
-      num)))
+; thanks agf for the algorithm: http://stackoverflow.com/a/6800214/198348
+(define (factors number)
+  (remove-duplicates
+    (flatten
+    (for/list ([divisor (range 1 (+ 1 (sqrt number)))]
+                #:when (eq? 0 (modulo number divisor)))
+      (list divisor (/ number divisor))))))
 
 (define (triangle increment number)
-  (let ([numfactors (length (simplefactors number))])
-    (if (eq? 0 (modulo number 100))
-      (printf "currently... increment: ~a, number: ~a, factors: ~a ~n" increment number numfactors)
-    ;(sleep 0.01)
-      (list))
+  (let ([numfactors (length (factors number))])
+    ;(if (eq? 0 (modulo number 1000))
+      ;(printf "currently... increment: ~a, number: ~a, factors: ~a ~n" increment number numfactors)
+      ;(list))
     (if (>= numfactors 500)
       (printf "found it: ~a ~n" number)
       (triangle (+ increment 1) (+ number increment)))))
-     
+
 (triangle 1 0)
+;(profile (triangle 1 0))
+;(length (factors 76576500))
