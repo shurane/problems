c6d1cd6b97c8fb034edb2140af2ae4b0bcd4a9cf
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-02-19 01:29:11 -0500

    no need to make it complicated. #19.
    But `(day-of-week year month day)` sounds complicated...

--- 19.scm
+++ 19.scm
@@ -1,8 +1,9 @@
 #lang racket
 
-(define years (range 1901 2000))
-(define months (range 1 12))
-(define (leap-year year)
+(define years (range 1901 (+ 2000 1)))
+(define months (range 1 (+ 12 1)))
+(define weekday (range 1 (+ 7 1)))
+(define (leap-year? year)
   (if (and
         (eq? (modulo year 4) 0)
         (not (and 
@@ -11,13 +12,10 @@
     #t
     #f))
 
-(define (day-of-week year month day)
-  'something)
-
-(define (days-in-months year month)
+(define (days-in-month year month)
   (cond
     [(eq? month 1 ) 31]
-    [(eq? month 2 ) (if (leap-year year) 29 28)]
+    [(eq? month 2 ) (if (leap-year? year) 29 28)]
     [(eq? month 3 ) 31]
     [(eq? month 4 ) 30]
     [(eq? month 5 ) 31]
@@ -30,3 +28,15 @@
     [(eq? month 12) 31]
     [else (error "shouldn't get here")]))
 
+(define ym (for*/list 
+             ([year years]
+              [month months])
+    (list year month (days-in-month year month))))
+
+(define ym-accum 
+  (reverse (foldl (lambda (i result) (cons 
+                            (+ (car result) (caddr i)) 
+                            result))
+       (list 0) ym)))
+
+(length (filter (lambda (i) (eq? (modulo i 7) 0)) ym-accum))
