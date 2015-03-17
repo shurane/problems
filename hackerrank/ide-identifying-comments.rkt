eb210bec4bf9a881d8cfe3ee02caf31c79f7fe41
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-03-17 14:21:31 -0400

    almost working

---
+++ hackerrank/ide-identifying-comments.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define (keep-reading)
+  (define line (read-line))
+  (if (eof-object? line)
+    (list)
+    (cons line (keep-reading))))
+
+(define (replace-line-comments s)
+  (regexp-replace* #rx"//.*?\n" s "\n"))
+(define (replace-block-comments s)
+  (regexp-replace* #rx"/\\*.*?\\*/" s ""))
+
+; use positive-lookahead
+(define (match-line-comments s)
+  (regexp-match* #rx"//.*?(?=\n)" s))
+(define (match-block-comments s)
+  (regexp-match* #rx"/\\*.*?\\*/" s))
+(define (match-both-comments s)
+  (regexp-match* #rx"//.*?(?=\n)|/\\*.*?\\*/" s))
+
+(define input (string-join (keep-reading) "\n"))
+(for ([comment (match-both-comments input)])
+  (displayln comment))
