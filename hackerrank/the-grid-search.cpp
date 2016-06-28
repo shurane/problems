308dd44469581f83116e45fcb6537ec410f839f4
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2016-06-28 10:24:19 -0400

    serious performance improvements
    It's not a good idea to create std::vectors thousands of times.
    malloc and free are expensive.

--- hackerrank/the-grid-search.cpp
+++ hackerrank/the-grid-search.cpp
@@ -3,41 +3,51 @@
 #include "prettyprint.hpp"
 
 std::vector<std::vector<int>> readmatrix(int r, int c){
-    std::vector<std::vector<int>> rows;
+    std::vector<std::vector<int>> rows(r);
     for (int ri=0; ri<r; ri++){
-        std::vector<int> row;
+        std::vector<int> row(c);
         std::string row_as_string;
         std::getline(std::cin, row_as_string);
         for (int i=0; i<row_as_string.size(); i++){
             // lol ascii
-            row.push_back(row_as_string[i] - '0');
+            row[i] = row_as_string[i] - '0';
         }
-        rows.push_back(row);
+        rows[ri] = row;
     }
     return rows;
 }
 
 // starts at (y,x) for a total of r rows and c columns
 std::vector<std::vector<int>> submatrix(
-        std::vector<std::vector<int>> matrix, 
-        int y, int x, 
+        std::vector<std::vector<int>> matrix,
+        int y, int x,
         int r, int c){
-    std::vector<std::vector<int>> sub;
+    std::vector<std::vector<int>> sub(r);
     for (int i=y; i<y+r; i++){
-        std::vector<int> sub_row;
+        std::vector<int> sub_row(c);
         for (int j=x; j<x+c; j++){
-            sub_row.push_back(matrix[i][j]);
+            sub_row[j-x] = matrix[i][j];
         }
-        sub.push_back(sub_row);
+        sub[i-y] = sub_row;
     }
     return sub;
 }
 
-bool matches(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
+bool matches2(std::vector<std::vector<int>>& a, int y, int x, int r, int c, std::vector<std::vector<int>>& pattern){
+    for (int i=y; i<y+r; i++){
+        for (int j=x; j<x+c; j++){
+            if (a[i][j] != pattern[i-y][j-x])
+                return false;
+        }
+    }
+    return true;
+}
+
+bool matches(std::vector<std::vector<int>>& a, std::vector<std::vector<int>>& b){
     if (a.size() != b.size())
         return false;
-    else if (a[0].size() > 0 && 
-             b[0].size() > 0 && 
+    else if (a[0].size() > 0 &&
+             b[0].size() > 0 &&
              a[0].size() != b[0].size())
         return false;
     else {
@@ -84,11 +94,19 @@ int main(int argc, char** argv){
         //}
 
         bool found_match = false;
-        for (int j=0; j<r-pr; j++){
-            for (int k=0; k<c-pc; k++){
-                if (matches(submatrix(rows, j, k, pr, pc), pattern)){
-                    //std::cout << "j: " << j << ",k: " << k 
-                              //<< ",pr: " << pr << ",pc: " << pc << std::endl;
+        for (int j=0; j<r-pr+1; j++){
+            for (int k=0; k<c-pc+1; k++){
+
+                //for (auto it=subm.begin(); it!=subm.end(); it++){
+                    //std::cout << *it << std::endl;
+                //}
+                //std::cout << "===========" << std::endl;
+
+                // wow, making this many vectors is *SLOW*
+                //auto subm = submatrix(rows, j, k, pr, pc);
+                //if (matches(subm, pattern)){
+
+                if (matches2(rows, j, k, pr, pc, pattern)){
                     //std::cout << submatrix(rows, j, k, pr, pc) << std::endl;
                     found_match = true;
                 }
