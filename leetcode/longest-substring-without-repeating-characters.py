d182ac03c7005e833d8010e21459e06599548ed6
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2016-08-28 16:03:16 -0400

    longest-substring-without-repeating-characters: O(n) and working

--- leetcode/longest-substring-without-repeating-characters.py
+++ leetcode/longest-substring-without-repeating-characters.py
@@ -1,19 +1,18 @@
-import time
-import blessings
-term = blessings.Terminal()
+# import blessings
+# term = blessings.Terminal()
 
-def colorizedStr(s, reds=[], blues=[], yellows=[]):
-    newS = ""
-    for j in range(len(s)):
-        if j in reds:
-            newS += term.red(s[j])
-        elif j in blues:
-            newS += term.blue(s[j])
-        elif j in yellows:
-            newS += term.yellow(s[j])
-        else:
-            newS += s[j]
-    return newS
+# def colorizedStr(s, reds=[], blues=[], yellows=[]):
+    # newS = ""
+    # for j in range(len(s)):
+        # if j in reds:
+            # newS += term.red(s[j])
+        # elif j in blues:
+            # newS += term.blue(s[j])
+        # elif j in yellows:
+            # newS += term.yellow(s[j])
+        # else:
+            # newS += s[j]
+    # return newS
 
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
@@ -25,33 +24,32 @@ class Solution(object):
         seen = {}
 
         maxString = ""
-        uniqueBegin = 0
+        begin = 0
 
         for i, letter in enumerate(s):
-            print "             {}"\
-                    .format(colorizedStr(s, reds=[i], yellows=range(uniqueBegin,i)))
+            # print "        {}, {}, {}"\
+                    # .format(colorizedStr(s, reds=[i], yellows=range(begin,i)), maxString, seen)
 
             # we are going to make sure the count of each letter is at most 1
             if letter in seen:
-                print "dupe of '{}': {}"\
-                        .format(letter, colorizedStr(s, blues=[seen[letter], i]))
-                # skip to one past the last seen occurrence of letter
-                uniqueBegin = seen[letter] + 1
+                if begin <= seen[letter]:
+                    # print "dupe {}: {}"\
+                            # .format(letter, colorizedStr(s, blues=[seen[letter], i]))
+                    # skip to one past the last seen occurrence of letter
+                    begin = seen[letter] + 1
 
             seen[letter] = i
 
-            sub = s[uniqueBegin:i+1]
+            sub = s[begin:i+1]
 
             if len(sub) > len(maxString):
                 maxString = sub
 
-        return maxString
+        return len(maxString)
 
-
-
-s = Solution()
-
-assert s.lengthOfLongestSubstring("abcabcbb") == "abc"
-assert s.lengthOfLongestSubstring("bbbbb") == "b"
-assert s.lengthOfLongestSubstring("pwwkew") == "wke"
+# s = Solution()
+# assert s.lengthOfLongestSubstring("abcabcbb") == 3
+# assert s.lengthOfLongestSubstring("bbbbb") == 1
+# assert s.lengthOfLongestSubstring("pwwkew") == 3
+# assert s.lengthOfLongestSubstring("abba") == 2
 
