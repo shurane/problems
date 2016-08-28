bd94959b7029145aefd91f0e3b68f09c5607b0a3
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2016-08-28 16:52:56 -0400

    shuffle: O(n), fisher-yates algorithm

---
+++ shuffle.py
@@ -0,0 +1,40 @@
+import random
+import blessings
+term = blessings.Terminal()
+
+# this looks suspiciously a lot like the fisher yates algorithm
+lst = range(30)
+
+
+def colorized(lst, yellows=[]):
+    result = "["
+    delim = ", "
+    for i in range(len(lst)):
+        if i == len(lst) - 1:
+            delim = ""
+        if i in yellows:
+            result += term.yellow(str(lst[i])) + delim
+        else:
+            result +=             str(lst[i])  + delim
+
+    result += "]"
+    return result
+
+def swap(lst, i, j):
+    temp = lst[i]
+    lst[i] = lst[j]
+    lst[j] = temp
+
+def shuffle(lst):
+    shuffled = lst[:]
+    n = len(lst)
+    i = 0
+    while i < n:
+        j = random.randrange(n-i)+i
+        swap(lst, i, j)
+        # print lst[:i], lst[i:]
+        print colorized(lst, yellows=range(i))
+
+        i += 1
+
+shuffle(lst)
