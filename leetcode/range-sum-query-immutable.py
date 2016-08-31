16603935187d78e782f47d5eefd5a0df528faf01
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2016-08-31 15:37:46 -0400

    range-sum-query: O(n)

--- leetcode/range-sum-query-immutable.py
+++ leetcode/range-sum-query-immutable.py
@@ -1,50 +1,20 @@
-import blessings
-term = blessings.Terminal()
-
-def colorized(lst, yellows=[]):
-    result = "["
-    delim = ", "
-    for i in range(len(lst)):
-        if i == len(lst) - 1:
-            delim = ""
-
-        v = "{: 3d}".format(lst[i])
-
-        if i in yellows:
-            result += term.yellow(v) + delim
-        else:
-            result +=             v  + delim
-
-    result += "]"
-    return result
-
 class NumArray(object):
     def __init__(self, nums):
         """
         initialize your data structure here.
         :type nums: List[int]
         """
-        # make an n by n grid to query (i,j)
-        n = len(nums)
-        self.sumranges = [[0 for j in xrange(n)] for i in xrange(n)]
-
         if not nums:
             return
 
-        self.sumranges[0][0] = nums[0]
+        n = len(nums)
+        self.sumranges = [0 for i in nums]
+        self.nums = nums
 
-        i = 0
-        while i < len(nums):
-            self.sumranges[i][i] = nums[i]
-            j = i+1
-            while j < len(nums):
-                self.sumranges[i][j] = self.sumranges[i][j-1] + nums[j]
-                j += 1
-            i += 1
+        self.sumranges[0] = nums[0]
 
-        for i in range(n):
-            print colorized(self.sumranges[i], yellows = range(i,n))
-        
+        for i in xrange(1,n):
+            self.sumranges[i] = self.sumranges[i-1] + nums[i]
 
     def sumRange(self, i, j):
         """
@@ -54,17 +24,40 @@ class NumArray(object):
         :rtype: int
         """
 
+        # the n-by-n grid was too much...
+        # the problem here is the n by n grid... it's O(n^2) space
+        # it chokes on an input of n=10,000
+        # so... what to do?
+
+        # there's a simpler recurrence relation that is linear
+
         # SR(i,j) = SR(i,j-1) + A[j]
-        # SR(0,0) = 0
         # SR(i,i) = A[i]
 
-        return self.sumranges[i][j]
-        
+
+        return self.sumranges[j] - self.sumranges[i] + self.nums[i]
 
 
-# Your NumArray object will be instantiated and called as such:
-nums = [-2, 0, 3, -5, 2, -1]
-numArray = NumArray(nums)
-print numArray.sumRange(0, 1)
-print numArray.sumRange(1, 2)
-print numArray.sumRange(0, 5)
+
+# nums = [-2, 0, 3, -5, 2, -1]
+# numArray = NumArray(nums)
+# assert numArray.sumRange(0, 2) == 1
+# assert numArray.sumRange(2, 5) == -1
+# assert numArray.sumRange(0, 5) == -3
+
+n2 = NumArray([1000,2])
+assert n2.sumRange(0,0) == 1000
+assert n2.sumRange(0,1) == 1002
+assert n2.sumRange(1,1) == 2
+
+# n3 = NumArray([10,20,30,40,50])
+# print n3.sumRange(0,0) # 10
+# print n3.sumRange(0,1) # 10 + 20 == 30
+# print n3.sumRange(0,2) # 10 + 20 + 30 == 60
+# print n3.sumRange(0,3) # 10 + 20 + 30 + 40 == 100
+# print n3.sumRange(0,4) # 10 + 20 + 30 + 40 + 50 == 150
+# print n3.sumRange(1,1) # 20
+# print n3.sumRange(1,2) # 20 + 30 == 50
+# print n3.sumRange(1,3) # 20 + 30 + 40 == 90
+# print n3.sumRange(1,4) # 20 + 30 + 40 + 50 == 140
+
