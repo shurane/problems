fd21331b1e1c944303241c7646e0342651d75d2e
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2016-09-01 19:16:22 -0400

    decode-ways: O(n) DP solution. It's kind of tricky vs recursio

--- leetcode/decode-ways.py
+++ leetcode/decode-ways.py
@@ -1,31 +1,10 @@
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 values = [str(ord(c) - 64) for c in letters]
-map = dict(zip(values, letters))
 
 def decode(digits):
-    return digits in map
+    return digits in values
 
 class Solution(object):
-
-    def numDecodings2(self, s):
-        # print "numDecodings2({})".format(s)
-        if len(s) == 0:
-            return 0
-        elif len(s) == 1:
-            return int(decode(s))
-        elif len(s) == 2:
-            return int(decode(s)) + int(decode(s[0]) and int(decode(s[1])))
-        else:
-            total = 0
-            if decode(s[-1:]):
-                # print "recurring on... {}".format(s[:-1])
-                total += self.numDecodings2(s[:-1])
-            if decode(s[-2:]):
-                # print "recurring on... {}".format(s[:-2])
-                total += self.numDecodings2(s[:-2])
-
-            return total
-
     def numDecodings(self, s):
         """
         :type s: str
@@ -37,60 +16,50 @@ class Solution(object):
         # A[1] = 1 or 2
         # A[0] = 1
         # It's a lot like fibonacci...
+        # the recursive solution is way easier to write than the iterate solution
 
         if len(s) == 0:
             return 0
         elif len(s) == 1:
             return int(decode(s))
         elif len(s) == 2:
-            return int(decode(s)) + int(decode(s[0]) and int(decode(s[1])))
+            return int(decode(s)) + int(decode(s[0]) and decode(s[1]))
 
         A = [0 for c in s]
         A[0] = int(decode(s[0]))
-
-        if A[0] == 0:
-            A[1] = 0
-        else:
-            total = 0
-            if decode(s[0]) and decode(s[1]):
-                total += 1
-            if decode(s[0:2]):
-                total += 1
-            A[1] = total
+        A[1] = int(decode(s[0:2])) + int(decode(s[0]) and decode(s[1]))
 
         for i in xrange(2, len(s)):
             total = 0
             
+            # print "checking {}".format(s[i])
             if decode(s[i]):
                 total += A[i-1]
             
-            if decode(s[i-2:i]):
+            # print "checking {}".format(s[i-1:i+1])
+            if decode(s[i-1:i+1]):
                 total += A[i-2]
 
-
-            if A[i-1] == 0 or not (decode(s[i]) and decode(s[i-2:i])):
-                A[i] = 0
-            else:
-                A[i] = total
-
-            print A
+            A[i] = total
 
         return A[-1]
-        
 
 s = Solution()
-assert s.numDecodings2("") == 0
-assert s.numDecodings2("1") == 1
-assert s.numDecodings2("9") == 1
-assert s.numDecodings2("10") == 1 # (10)
-assert s.numDecodings2("11") == 2 # (11), (1, 1)
-assert s.numDecodings2("12") == 2 # (12), (1, 2)
-assert s.numDecodings2("26") == 2 # (26), (2, 6)
-assert s.numDecodings2("27") == 1 # (27)
-assert s.numDecodings2("100") == 0 
-assert s.numDecodings2("111") == 3 # (11, 1), (1, 11), (1, 1, 1)
-assert s.numDecodings2("666") == 1 # (6, 6, 6)
-assert s.numDecodings2("777") == 1 # (7, 7, 7)
-assert s.numDecodings2("007") == 0
-assert s.numDecodings2("1010") == 1
-assert s.numDecodings2("10101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010") == 1
+assert s.numDecodings("") == 0
+assert s.numDecodings("1") == 1
+assert s.numDecodings("9") == 1
+assert s.numDecodings("10") == 1 # (10)
+assert s.numDecodings("11") == 2 # (11), (1, 1)
+assert s.numDecodings("12") == 2 # (12), (1, 2)
+assert s.numDecodings("26") == 2 # (26), (2, 6)
+assert s.numDecodings("27") == 1 # (27)
+assert s.numDecodings("63") == 1 # (6,3)
+assert s.numDecodings("77") == 1 # (7,7)
+assert s.numDecodings("100") == 0 
+assert s.numDecodings("111") == 3 # (11, 1), (1, 11), (1, 1, 1)
+assert s.numDecodings("666") == 1 # (6, 6, 6)
+assert s.numDecodings("777") == 1 # (7, 7, 7)
+assert s.numDecodings("007") == 0
+assert s.numDecodings("1010") == 1
+assert s.numDecodings("10101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010") == 1
+print s.numDecodings("111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
