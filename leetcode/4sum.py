dc9c75b9aa3fe511ede6851df66cb5692c9718fd
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2017-05-20 18:19:43 -0700

    4sum: useful slow but correct solution

--- leetcode/4sum.py
+++ leetcode/4sum.py
@@ -1,4 +1,6 @@
+import random
 
+random.seed(0)
 
 class Solution(object):
 
@@ -18,6 +20,7 @@ class Solution(object):
         """
 
         #brute force approach would be n**4, 4 for loops
+        # at least it's useful to know that it's correct
 
         n = len(nums)
         quadruplets = []
@@ -28,8 +31,11 @@ class Solution(object):
                         if nums[i] + nums[j] + nums[k] + nums[l] == target:
                             quadruplets.append([nums[i],nums[j],nums[k],nums[l]])
         return quadruplets
-        
+
 
 s = Solution()
+# print s.fourSumSlow([1, 0, -1, 0, -2, 2], 0)
 
-print s.fourSumSlow([1, 0, -1, 0, -2, 2], 0)
+randnums = [random.randint(-50, 50) for i in range(100000)]
+randtarget = random.randint(0,100)
+print s.fourSumSlow(randnums, randtarget)
