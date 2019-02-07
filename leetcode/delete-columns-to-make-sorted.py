f2fabf41dda3ddcd2aff6ac6321744025e1de6b3
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2019-02-07 14:30:45 -0800

    delete-columns-to-make-sorted: O(M*N) solution
    M = length of word, N = number of words.
    
    Alternative solution that's pretty clever but looks like O(M * N log N): 
    https://leetcode.com/problems/delete-columns-to-make-sorted/discuss/198881/Fast-and-Easy-to-Understand-Python-Solution

---
+++ leetcode/delete-columns-to-make-sorted.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minDeletionSize(self, A: 'List[str]') -> 'int':
+        length = len(A)
+        width = len(A[0])
+
+        indexes = []
+        for i in range(width):
+            # check two words at a time
+            for j in range(length - 1):
+                if A[j][i] <= A[j+1][i]:
+                    pass
+                else:
+                    indexes.append(i)
+                    break
+        # print(indexes)
+        return len(indexes)
+
+s = Solution()
+
+assert s.minDeletionSize(["b","a"]) == 1 # is this valid? then it just becomes ["", ""]
+assert s.minDeletionSize(["a","b"]) == 0
+assert s.minDeletionSize(["cba","daf","ghi"]) == 1
+assert s.minDeletionSize(["zyx","wvu","tsr"]) == 3
+
+# 6 minutes
