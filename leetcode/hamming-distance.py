4aa26ac7696c900dd593f222dd816d228cf7d4c6
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2019-02-07 15:50:55 -0800

    hamming-distance: O(n) with XOR, &, and >> operators
    Didn't realize I did this before. Redoing with slightly different solutions.

--- leetcode/hamming-distance.py
+++ leetcode/hamming-distance.py
@@ -17,6 +17,24 @@ class Solution(object):
             x = x >> 1
             y = y >> 1
         return diff
+    
+    def hammingDistance2(self, x: 'int', y: 'int') -> 'int':
+        # print(format(x,'032b'), format(y,'032b'))
+
+        count = 0
+        for a, b in zip(format(x, '032b'), format(y, '032b')):
+            if a != b:
+                count += 1
+        return count
+
+    def hammingDistance3(self, x: 'int', y: 'int') -> 'int':
+        # print(format(x,'032b'), format(y,'032b'))
+        result = x ^ y
+        count = 0
+        while result > 0:
+            count += result & 1
+            result = result >> 1
+        return count
 
 s = Solution()
 
