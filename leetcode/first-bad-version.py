6a84c89f9afc1889b103ef8deae5a64fc8999ed1
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2019-05-24 18:35:22 -0700

    first-bad-version: O(log n)

---
+++ leetcode/first-bad-version.py
@@ -0,0 +1,24 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lo, hi = 0, n
+        
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if isBadVersion(mid):
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        
+        if not isBadVersion(mid):
+            return mid + 1
+        else:
+            return mid
