33cb7e937042355c7d250fd72101fdc86ba034e3
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2019-09-02 21:01:44 -0700

    two-sum-ii: 2 pointers method

--- leetcode/two-sum-ii-input-array-is-sorted.py
+++ leetcode/two-sum-ii-input-array-is-sorted.py
@@ -17,6 +17,18 @@ class Solution:
 
         return None
 
+    def twoSumTwoPointers(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers) - 1
+        while l < r:
+            if numbers[l] + numbers[r] < target:
+                l += 1
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                return [l+1, r+1]
+        return None
+
 def bsearch(numbers, goal, lo=None, hi=None):
     if lo == None:
         lo = 0
@@ -41,6 +53,12 @@ s = Solution()
 
 # assert s.twoSum([2,7,11,15], 9) == [1,2]
 # assert s.twoSum([2,3,4], 6) == [1,3]
-assert s.twoSum([5,25,75], 100) == [2, 3]
-assert s.twoSum([1,2,3,4,4,9,56,90], 8) == [4, 5]
-assert s.twoSum([1] + [2 for i in range(1000000)] + [5 for i in range(1000000)] + [19], 20) == [1, 2000002]
+# assert s.twoSum([5,25,75], 100) == [2, 3]
+# assert s.twoSum([1,2,3,4,4,9,56,90], 8) == [4, 5]
+# assert s.twoSum([1] + [2 for i in range(1000000)] + [5 for i in range(1000000)] + [19], 20) == [1, 2000002]
+
+assert s.twoSumTwoPointers([2,7,11,15], 9) == [1,2]
+assert s.twoSumTwoPointers([2,3,4], 6) == [1,3]
+assert s.twoSumTwoPointers([5,25,75], 100) == [2, 3]
+assert s.twoSumTwoPointers([1,2,3,4,4,9,56,90], 8) == [4, 5]
+# assert s.twoSumTwoPointers([1] + [2 for i in range(1000000)] + [5 for i in range(1000000)] + [19], 20) == [1, 2000002]
