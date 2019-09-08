4317e61703e13bf7e14d1120712b4776bf1ccf92
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2019-09-08 02:20:25 -0700

    product-of-array-except-self: more tests

--- leetcode/product-of-array-except-self.py
+++ leetcode/product-of-array-except-self.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class Solution:
-    def productExceptSelfSlow(self, nums: List[int]) -> List[int]:
+    def productExceptSelfMoreMemory(self, nums: List[int]) -> List[int]:
         results = [1 for num in nums]
         lefts   = [1 for num in nums]
         rights  = [1 for num in nums]
@@ -32,7 +32,6 @@ class Solution:
 
         return results
 
-
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # ok, this is O(1) per the solution, but... could be done more neatly
         results = [1 for num in nums]
@@ -58,7 +57,22 @@ class Solution:
         print("after :", results)
         return results
 
+    def productExceptSelfNeater(self, nums: List[int]) -> List[int]:
+        # hints from https://leetcode.com/problems/product-of-array-except-self/discuss/65622/Simple-Java-solution-in-O(n)-without-extra-space
+        results = [1 for num in nums]
+
+        for i in range(1, len(nums)):
+            results[i] = results[i-1] * nums[i-1]
+
+        rolling = 1
+        for j in range(len(nums) - 1, -1, -1):
+            results[j] *= rolling
+            rolling *= nums[j]
+
+        return results
+
+
 s = Solution()
-assert s.productExceptSelf([1,2,3,4]) == [24,12,8,6]
-assert s.productExceptSelf([1,2]) == [2,1]
-assert s.productExceptSelf([20,13]) == [13,20]
+assert s.productExceptSelfNeater([1,2,3,4]) == [24,12,8,6]
+assert s.productExceptSelfNeater([1,2]) == [2,1]
+assert s.productExceptSelfNeater([20,13]) == [13,20]
