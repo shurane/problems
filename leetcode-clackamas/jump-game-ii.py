91ede8c03923a80791a42ff496ea76c1e0e22656
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2021-05-05 22:34:08 -0700

    extra test case

--- leetcode-clackamas/jump-game-ii.py
+++ leetcode-clackamas/jump-game-ii.py
@@ -5,6 +5,7 @@ class Solution:
         dp = [len(nums) for i in nums]
         dp[-1] = 0
 
+        # print("  nums", nums)
         # print("before", dp)
         for i in range(len(nums) - 2, -1, -1):
             jump = nums[i]
@@ -14,11 +15,12 @@ class Solution:
                 dp[i] = min(dp[j] for j in possibleJumps) + 1
             else:
                 dp[i] = len(nums)
-        # print( "after", dp)
+        # print( " after", dp)
 
         return dp[0]
 
 s = Solution()
+
 assert s.jump([5,4,3,2,1,0]) == 1
 assert s.jump([4,4,3,2,1,0]) == 2
 assert s.jump([3,3,3,2,1,0]) == 2
@@ -41,3 +43,4 @@ assert s.jump([2,0,2,0,2,0]) == 3
 
 assert s.jump([2,3,1,1,4]) == 2
 assert s.jump([2,3,0,1,4]) == 2
+assert s.jump([1,3,2,3,1,0]) == 3
