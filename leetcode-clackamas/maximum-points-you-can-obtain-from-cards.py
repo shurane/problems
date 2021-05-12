e95ac8c69119809e5506052aba56863811c3562e
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2021-05-11 22:24:07 -0700

    few more lines

--- leetcode-clackamas/maximum-points-you-can-obtain-from-cards.py
+++ leetcode-clackamas/maximum-points-you-can-obtain-from-cards.py
@@ -1,9 +1,7 @@
 from typing import List
-import sys
-sys.setrecursionlimit(10000)
 
 class Solution:
-    def maxScore(self, cardPoints: List[int], k: int) -> int:
+    def maxScoreRecursive(self, cardPoints: List[int], k: int) -> int:
         cache = dict()
         score = self.helper(cardPoints, k, 0, len(cardPoints) - 1, cache)
         # print(score)
@@ -20,7 +18,7 @@ class Solution:
             cache[(l,r)] = score
             return score
 
-    def maxScore2(self, cardPoints: List[int], k: int) -> int:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
         l = 0
         r = len(cardPoints) - k
         left = 0
@@ -30,8 +28,7 @@ class Solution:
         for i in range(0, k):
             left += cardPoints[l + i]
             right -= cardPoints[r + i]
-            if left + right > score:
-                score = left + right
+            score = max(score, left + right)
             # print(f"     loop {left:3}, {right:3}, {left + right:4}, {l+i:3}, {r+i:3}")
 
         # print("final score", score)
@@ -39,20 +36,20 @@ class Solution:
 
 s = Solution()
 
-assert s.maxScore2([1,2,3,4,5,6], 3) == 15
-assert s.maxScore2([6,5,4,3,2,1], 3) == 15
+assert s.maxScore([1,2,3,4,5,6], 3) == 15
+assert s.maxScore([6,5,4,3,2,1], 3) == 15
 
-assert s.maxScore2([1,2,3,4,5,6,1], 3) == 12
-assert s.maxScore2([2,2,2], 2) == 4
-assert s.maxScore2([9,7,7,9,7,7,9], 7) == 55
-assert s.maxScore2([1,1000,1], 1) == 1
-assert s.maxScore2([1,79,80,1,1,1,200,1], 3) == 202
-assert s.maxScore2([50,40,17,25,100,75], 3) == 225
-assert s.maxScore2([100,40,17,9,73,75], 3) == 248
+assert s.maxScore([1,2,3,4,5,6,1], 3) == 12
+assert s.maxScore([2,2,2], 2) == 4
+assert s.maxScore([9,7,7,9,7,7,9], 7) == 55
+assert s.maxScore([1,1000,1], 1) == 1
+assert s.maxScore([1,79,80,1,1,1,200,1], 3) == 202
+assert s.maxScore([50,40,17,25,100,75], 3) == 225
+assert s.maxScore([100,40,17,9,73,75], 3) == 248
 
 import random
 random.seed(0)
 lst = [random.randint(1, 1000) for i in range(10000)]
 k = len(lst) // 2
 # print(k, lst[:100])
-assert s.maxScore2(lst, k) == 2528779
+assert s.maxScore(lst, k) == 2528779
