ada5145bfaea799092805aa75c1d79000066021a
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2021-05-11 22:22:54 -0700

    removed a few lines, enabled other test cases

--- leetcode-clackamas/maximum-points-you-can-obtain-from-cards.py
+++ leetcode-clackamas/maximum-points-you-can-obtain-from-cards.py
@@ -21,34 +21,32 @@ class Solution:
             return score
 
     def maxScore2(self, cardPoints: List[int], k: int) -> int:
-        l = -1
-        r = len(cardPoints) - k - 1
+        l = 0
+        r = len(cardPoints) - k
         left = 0
-        right = sum(cardPoints[r+1:])
+        right = sum(cardPoints[r:])
         score = left + right
         # print(f"beginning {left:3}, {right:3}, {score:4}, {l:3}, {r:3}, {cardPoints[r:]}")
-        for _ in range(0, k):
-            l += 1
-            r += 1
-            left += cardPoints[l]
-            right -= cardPoints[r]
+        for i in range(0, k):
+            left += cardPoints[l + i]
+            right -= cardPoints[r + i]
             if left + right > score:
                 score = left + right
-            # print(f"     loop {left:3}, {right:3}, {left + right:4}, {l:3}, {r:3}")
+            # print(f"     loop {left:3}, {right:3}, {left + right:4}, {l+i:3}, {r+i:3}")
 
         # print("final score", score)
         return score
 
 s = Solution()
 
-# assert s.maxScore2([1,2,3,4,5,6], 3) == 15
-# assert s.maxScore2([6,5,4,3,2,1], 3) == 15
+assert s.maxScore2([1,2,3,4,5,6], 3) == 15
+assert s.maxScore2([6,5,4,3,2,1], 3) == 15
 
-# assert s.maxScore2([1,2,3,4,5,6,1], 3) == 12
-# assert s.maxScore2([2,2,2], 2) == 4
-# assert s.maxScore2([9,7,7,9,7,7,9], 7) == 55
-# assert s.maxScore2([1,1000,1], 1) == 1
-# assert s.maxScore2([1,79,80,1,1,1,200,1], 3) == 202
+assert s.maxScore2([1,2,3,4,5,6,1], 3) == 12
+assert s.maxScore2([2,2,2], 2) == 4
+assert s.maxScore2([9,7,7,9,7,7,9], 7) == 55
+assert s.maxScore2([1,1000,1], 1) == 1
+assert s.maxScore2([1,79,80,1,1,1,200,1], 3) == 202
 assert s.maxScore2([50,40,17,25,100,75], 3) == 225
 assert s.maxScore2([100,40,17,9,73,75], 3) == 248
 
