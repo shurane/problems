576069031b3537c4da773aabcc8084534351018e
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2021-10-06 20:28:33 -0700

    tech interview handbook work

--- leetcode/product-of-array-except-self.py
+++ leetcode/product-of-array-except-self.py
@@ -71,8 +71,40 @@ class Solution:
 
         return results
 
+    def productExceptSelfRedux(self, nums: List[int]) -> List[int]:
+        # was on the right track, but got some help from this
+        # https://leetcode.com/problems/product-of-array-except-self/discuss/65625/Python-solution-(Accepted)-O(n)-time-O(1)-space
+
+        results = [1 for i in nums]
+        l = 1
+        for i, n in enumerate(nums):
+            results[i] = l
+            l *= n
+        # print("pass 1", results)
+        r = 1
+        for i, n in enumerate(reversed(nums)):
+            results[~i] *= r
+            r *= n
+        # print("pass 2", results)
+
+        return results
 
 s = Solution()
+
 assert s.productExceptSelfNeater([1,2,3,4]) == [24,12,8,6]
 assert s.productExceptSelfNeater([1,2]) == [2,1]
 assert s.productExceptSelfNeater([20,13]) == [13,20]
+l1 = list(range(1,12))
+l2 = list(1 for i in range(31))
+l3 = list(2 for i in range(31))
+# try the problem with 1 or 2 zeroes. Makes it difficult if you're using division, but otherwise doesn't break a sweat
+l4 = l1[:]; l4[0] = 0
+l5 = l1[:]; l5[0] = 0; l5[1] = 0
+assert s.productExceptSelfRedux(l1) == s.productExceptSelfNeater(l1)
+assert s.productExceptSelfRedux(l2) == s.productExceptSelfNeater(l2)
+assert s.productExceptSelfRedux(l3) == s.productExceptSelfNeater(l3)
+print(s.productExceptSelfRedux(l1))
+print(s.productExceptSelfRedux(l2))
+print(s.productExceptSelfRedux(l3))
+print(s.productExceptSelfRedux(l4))
+print(s.productExceptSelfRedux(l5))
