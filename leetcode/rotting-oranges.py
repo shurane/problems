05e71edb0b693c98b1165aeb18a9042ec1bbb1b9
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-01-19 21:26:41 -0800

    slight optimization and slightly easier to read

--- leetcode/rotting-oranges.py
+++ leetcode/rotting-oranges.py
@@ -5,7 +5,7 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         fresh = 0
-        minutes = -1
+        minutes = 0
         rottenQ = []
 
         for i in range(m):
@@ -15,19 +15,25 @@ class Solution:
                 elif grid[i][j] == 2:
                     rottenQ.append((i, j))
 
-        while rottenQ:
-            minutes += 1
+        # # https://stackoverflow.com/a/59123981/198348 for formatting a 2D matrix
+        # print("matrix")
+        # print(*(" ".join([str(c) for c in row]) for row in grid), sep="\n")
+
+        while rottenQ and fresh > 0:
             nextRottenQ = []
+            # print(f"minutes:{minutes}, fresh:{fresh}, rottenQ:{rottenQ}")
             for i, j in rottenQ:
                 for ni, nj in [(0,1),(0,-1),(1,0),(-1,0)]:
                     if 0 <= i + ni < m and 0 <= j + nj < n and grid[i+ni][j+nj] == 1:
                         nextRottenQ.append((i + ni, j + nj))
                         grid[i+ni][j+nj] = 2
                         fresh -= 1
+            if nextRottenQ:
+                minutes += 1
             rottenQ = nextRottenQ
 
         if fresh == 0:
-            return max(minutes, 0)
+            return minutes
         return -1
 
 s = Solution()
