3c7abceb513fb290d5096102622c2f4923970ded
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-02-08 02:46:50 -0800

    linear solution using a strictly decreasing stack

--- leetcode/maximum-binary-tree.py
+++ leetcode/maximum-binary-tree.py
@@ -3,30 +3,25 @@ from helpers2 import TreeNode, create_tree
 
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        def helper(begin, end):
-            if begin == end:
-                return None
-            elif begin + 1 == end:
-                return TreeNode(nums[begin])
+        # linear based on https://leetcode.com/problems/maximum-binary-tree/discuss/106146/C%2B%2B-O(N)-solution
+        # same, but in python: https://leetcode.com/problems/maximum-binary-tree/discuss/258364/Python-O(n)-solution-with-explanation.
+        # also related: https://leetcode.com/problems/maximum-binary-tree/discuss/106147/c-8-lines-on-log-n-map-plus-stack-with-binary-search
+        # sentinel value https://leetcode.com/problems/maximum-binary-tree/discuss/106146/C++-O(N)-solution/108588
+        stack = [TreeNode(2**32 - 1)]
 
-            index = value = -1
+        for num in nums:
+            t = TreeNode(num)
+            while stack[-1].val < num:
+                t.left = stack[-1]
+                stack.pop()
+            stack[-1].right = t
+            stack.append(t)
 
-            # how to stop scanning a second time? Essentially doing 2*N iterations here
-            # this reminds me of quick sort. We look for a pivot, then end up scanning the left and right sides again
-            for i in range(begin, end):
-                if nums[i] > value:
-                    index = i
-                    value = nums[i]
-
-            t = TreeNode(nums[index])
-            t.left = helper(begin, index)
-            t.right = helper(index+1, end)
-
-            return t
-
-        return helper(0, len(nums))
+        return stack[1]
 
 s = Solution()
 assert s.constructMaximumBinaryTree([3,2,1]) == create_tree([3,None,2,None,None,1])
 assert s.constructMaximumBinaryTree([1,2,3]) == create_tree([3,2,None,1])
 assert s.constructMaximumBinaryTree([3,2,1,6,0,5]) == create_tree([6,3,5,None,2,0,None,None,1])
+assert s.constructMaximumBinaryTree([3,1,2,6,0,5]) == create_tree([6,3,5,None,2,0,None,None,1])
+assert s.constructMaximumBinaryTree([2,1,3,6,0,5]) == create_tree([6,3,5,2,None,0,None,None,1])
