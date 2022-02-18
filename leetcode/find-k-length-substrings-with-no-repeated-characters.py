bdf092d62014038d04b64f466e81641d966a18c0
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-02-17 16:41:49 -0800

    potw: k length substrings without repeated characters, cleaner

--- leetcode/find-k-length-substrings-with-no-repeated-characters.py
+++ leetcode/find-k-length-substrings-with-no-repeated-characters.py
@@ -36,10 +36,37 @@ class Solution:
 
         return results
 
+    # https://leetcode.com/problems/find-k-length-substrings-with-no-repeated-characters/discuss/1776611/Python-Sliding-windows-fun
+    def numKLenSubstrNoRepeatsSimpler(self, s: str, k: int) -> int:
+        count = defaultdict(int)
+        res = 0
+        dupes = 0
+
+        for i in range(len(s)):
+            count[s[i]] += 1
+            if count[s[i]] == 2:
+                dupes += 1
+
+            if i-k>=0:
+                count[s[i-k]] -= 1
+                if count[s[i-k]] == 1:
+                    dupes -= 1
+
+            res += i-k+1>=0 and dupes == 0
+        return res
+
 s = Solution()
+
 assert s.numKLenSubstrNoRepeats("abcdef", 1) == 6
 assert s.numKLenSubstrNoRepeats("abcdef", 2) == 5
 assert s.numKLenSubstrNoRepeats("aabbccddeeff", 2) == 5
 assert s.numKLenSubstrNoRepeats("zaabbccddeeffg", 2) == 7
 assert s.numKLenSubstrNoRepeats("havefunonleetcode", 5) == 6
 assert s.numKLenSubstrNoRepeats("home", 5) == 0
+
+assert s.numKLenSubstrNoRepeatsSimpler("abcdef", 1) == 6
+assert s.numKLenSubstrNoRepeatsSimpler("abcdef", 2) == 5
+assert s.numKLenSubstrNoRepeatsSimpler("aabbccddeeff", 2) == 5
+assert s.numKLenSubstrNoRepeatsSimpler("zaabbccddeeffg", 2) == 7
+assert s.numKLenSubstrNoRepeatsSimpler("havefunonleetcode", 5) == 6
+assert s.numKLenSubstrNoRepeatsSimpler("home", 5) == 0
