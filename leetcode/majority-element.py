3726dffbedee6e8963ea2a8b5973b7a30164866c
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-02-22 10:49:50 -0800

    potd: majority-element, dict, boyer-moores

--- leetcode/majority-element.py
+++ leetcode/majority-element.py
@@ -1,50 +1,46 @@
 from typing import List
+import random
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # base case with 1 element
-        # otherwise, sort the list and count the number of elements
-        # if count for a value is greater than n // 2, then return that
-        # run time: O(nlogn), space: O(1)
-
-        if len(nums) == 1:
-            return nums[0]
-
-        ordered = sorted(nums)
-        i = 0
-        count = 0
-        while i < len(ordered) - 1:
-            if ordered[i] != ordered[i+1]:
-                count = 0
-            count += 1
-
-            if count > len(nums) // 2:
-                return ordered[i]
-
-            i += 1
-        return None
-
-    def majorityElement2nd(self, nums: List[int]) -> int:
-        # Uses O(n) space in exchange for amortized O(n) time
         counts = dict()
-        for element in nums:
-            if element in counts:
-                counts[element] += 1
+        best = nums[0]
+        for n in nums:
+            if n not in counts:
+                counts[n] = 1
             else:
-                counts[element] = 1
+                counts[n] += 1
+            if counts[n] > counts[best]:
+                best = n
+        return best
 
-            if counts[element] > len(nums) // 2:
-                return element
+    # https://leetcode.com/problems/majority-element/solution/
+    # What is the Boyer-Moore Voting Algorithm? Very simple looking.
+    # But how do you prove that it is true?
+    def majorityElementBoyerMoore(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
 
-        return None
+        for num in nums:
+            if count == 0: candidate = num
+
+            if num == candidate: count += 1
+            else: count -= 1
+
+        return candidate
 
 s = Solution()
+assert s.majorityElement([3,2,3]) == 3
+assert s.majorityElement([2,2,1,1,1,2,2]) == 2
+assert s.majorityElementBoyerMoore([3,2,3]) == 3
+assert s.majorityElementBoyerMoore([2,2,1,1,1,2,2]) == 2
 
-assert s.majorityElement2nd([0]) == 0
-assert s.majorityElement2nd([3,2,3]) == 3
-assert s.majorityElement2nd([2,2,1,1,1,2,2]) == 2
+l1 = [2 for _ in range(50)] + [3 for _ in range(49)]
+l2 = [2 for _ in range(50)] + [3 for _ in range(51)]
+random.shuffle(l1)
+random.shuffle(l2)
 
-# [1,1,1,1,2,2,3,3] would not be a valid input.
-# A majority element would show up **more than** n // 2 times
-# note: https://leetcode.com/problems/majority-element/discuss/51613/O(n)-time-O(1)-space-fastest-solution
-# very cool solution for O(1) space and O(n) time, based on "Boyer-Moore Majority Vote Algorithm"
+assert s.majorityElement(l1) == 2
+assert s.majorityElement(l2) == 3
+assert s.majorityElementBoyerMoore(l1) == 2
+assert s.majorityElementBoyerMoore(l2) == 3
