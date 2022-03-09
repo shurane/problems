55e904444999dbfca1925ad9e05b414269627535
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-03-08 23:45:20 -0800

    potd: solution with less lines

--- leetcode/remove-duplicates-from-sorted-list-ii.py
+++ leetcode/remove-duplicates-from-sorted-list-ii.py
@@ -25,9 +25,26 @@ class Solution:
 
         return dummy.next
 
+    # https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/28336/Python-in-place-solution-with-dummy-head-node.
+    def deleteDuplicatesSimpler(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = pre = ListNode(0, head)
+
+        while head and head.next:
+            if head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                pre.next = head
+            else:
+                pre = head
+                head = head.next
+
+        return dummy.next
+
 s = Solution()
 
 assert s.deleteDuplicates(ListNode.fromList([])) == ListNode.fromList([])
+assert s.deleteDuplicatesSimpler(ListNode.fromList([])) == ListNode.fromList([])
 assert s.deleteDuplicates(ListNode.fromList([1])) == ListNode.fromList([1])
 assert s.deleteDuplicates(ListNode.fromList([1,2])) == ListNode.fromList([1,2])
 assert s.deleteDuplicates(ListNode.fromList([1,2,3,4,5])) == ListNode.fromList([1,2,3,4,5])
@@ -35,4 +52,7 @@ assert s.deleteDuplicates(ListNode.fromList([1,2,3,4,5,5])) == ListNode.fromList
 assert s.deleteDuplicates(ListNode.fromList([1,1,2,3,4,5])) == ListNode.fromList([2,3,4,5])
 assert s.deleteDuplicates(ListNode.fromList([1,2,3,3,4,5])) == ListNode.fromList([1,2,4,5])
 assert s.deleteDuplicates(ListNode.fromList([1,1,1,2,2,3])) == ListNode.fromList([3])
+assert s.deleteDuplicates(ListNode.fromList([1,1,2,2,3,3])) == ListNode.fromList([])
+assert s.deleteDuplicatesSimpler(ListNode.fromList([1,1,2,2,3,3])) == ListNode.fromList([])
 assert s.deleteDuplicates(ListNode.fromList([1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7])) == ListNode.fromList([1])
+assert s.deleteDuplicatesSimpler(ListNode.fromList([1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7])) == ListNode.fromList([1])
