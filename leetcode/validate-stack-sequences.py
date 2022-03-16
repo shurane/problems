1dc9a1dbb47ba3439199e6aa3baedeb8df05385c
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-03-15 23:01:25 -0700

    potd: validate-stack-sequences, O(1) space but slower solution

--- leetcode/validate-stack-sequences.py
+++ leetcode/validate-stack-sequences.py
@@ -30,14 +30,33 @@ class Solution:
 
         return stack == [-1]
 
+    def validateStackSequencesConstantSpace(self, pushed: List[int], popped: List[int]) -> bool:
+        i = 0
+        j = 0
+        while i < len(pushed):
+            # print("outer", pushed[i])
+            ii = i
+            while j < len(popped) and (pushed[ii] == popped[j] or pushed[ii] == -1):
+                # print("inner", pushed[ii], popped[j])
+                if pushed[ii] == popped[j]:
+                    pushed[ii] = -1
+                    j += 1
+                ii -= 1
+            i += 1
+        # print("final", i, j, "expected:", len(pushed))
+        return j == len(popped)
+
 s = Solution()
 
 testcases = [[[1,2,3], [3,2,1], True],
              [[1,2,3,4,5], [4,5,3,2,1], True],
              [[1,2,3,4,5], [4,3,5,1,2], False],
              [[2,1,0], [1,2,0], True],
-             [[0], [0], True]]
+             [[0], [0], True],
+             [[0,1,2,3,4,5,6,7,8,9], [2,1,4,3,6,5,8,7,9,0], True],
+             [[0,1,2,3,4,5,6,7,8,9], [3,6,9,8,7,5,4,2,1,0], True]]
 
 for pushed, popped, expected in testcases:
     assert s.validateStackSequences(pushed, popped) == expected
     assert s.validateStackSequencesLoop(pushed, popped) == expected
+    assert s.validateStackSequencesConstantSpace(pushed, popped) == expected
