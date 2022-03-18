1f26e932637a98e184b40fbb4f0b38d181c19bb1
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-03-18 02:08:36 -0700

    copy-list-with-random-pointer: cleanup, type hints, test cases, and optional output

--- leetcode/copy-list-with-random-pointer.py
+++ leetcode/copy-list-with-random-pointer.py
@@ -1,9 +1,9 @@
-from helpers2 import ListNode
-from typing import Dict
+# from helpers2 import ListNode
+from typing import Dict, Sequence, Any, Optional
 
 """slightly different LinkedList node with random pointer"""
 class Node:
-    def __init__(self, val, next, random):
+    def __init__(self, val, next: Optional['Node'] = None, random: Optional['Node'] = None):
         self.val = val
         self.next = next
         self.random = random
@@ -18,56 +18,94 @@ class Node:
         while c:
             s += f" -> {c.val}"
             c = c.next
-
         return s
+    def __hash__(self):
+        return hash(self.val)
+    @classmethod
+    def fromList(cls, lst: Sequence[Any]) -> Optional['Node']:
+        head = Node(0)
+        current = head
+        for elem in lst:
+            current.next = Node(elem)
+            current = current.next
+        return head.next
 
 class Solution:
-    def copyRandomList(self, head: 'Node'):
-        lookup: Dict[int, 'Node'] = dict()
-
-        copy = Node(None, None, None)
+    # not possible in 1 pass, but possible in 2 passes with O(n) extra space or 3 passes with O(1) extra space
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        lookup: Dict[Node, Node] = dict()
+        copy = Node(0)
 
         l = head
         r = copy
         while l:
-            r.next = Node(l.val, None, None)
+            r.next = Node(l.val)
             r = r.next
-            lookup[id(l)] = r
+            lookup[l] = r
             l = l.next
 
         l = head
         r = copy.next
-        while l:
+        while l and r:
             if l.random:
-                r.random = lookup[id(l.random)]
+                r.random = lookup[l.random]
 
             l = l.next
             r = r.next
 
-        return copy.next
+        return copy.next # type: ignore
 
-    # TODO
-    # [ ] is there a way to do this without id() or getting the address of the node?
-    #   - i.e. just marking the nodes from 1 to n?
-    # [ ] is there a way to do this in a single pass?
+    # https://leetcode.com/problems/copy-list-with-random-pointer/discuss/43485/Clear-and-short-python-O(2n)-and-O(n)-solution
+    def copyRandomListSimpler(self, head: 'Node') -> 'Node':
+        dic = dict()
+        m = n = head
+        while m:
+            dic[m] = Node(m.val)
+            m = m.next
+        while n:
+            dic[n].next = dic.get(n.next)
+            dic[n].random = dic.get(n.random)
+            n = n.next
+        return dic.get(head)
 
+def compareEqualAndCopied(l: Node, r: Node):
+    o, c = l, r
+    while o and c:
+        if id(o) == id(c): return False
+        elif o.val != c.val: return False
+        elif o.random is not None and c.random is not None:
+            if id(o.random) == id(c.random): return False
+            elif o.random.val != c.random.val: return False
+        o, c = o.next, c.next
+    return o is None and c is None
 
 s = Solution()
 
-t1 = ListNode.fromArgs(1,2)
-t1.random = t1.next
-t1.next.random = t1.next
+testcases = [[[1,2],[0,0]],
+             [[1],[None]],
+             [[3,3,3],[None,0,None]],
+             [[0,1,2,3,4,5,6,7,8,9],[9,0,1,2,3,4,5,6,7,8]]]
 
-t2 = ListNode.fromArgs(1)
-t2.random = None
+for order, random in testcases:
+    lookup = dict()
+    rlookup = dict()
+    orig = Node.fromList(order)
+    if not orig: continue
 
+    for i, node in enumerate(orig):
+        lookup[i] = node
+        rlookup[node] = i
 
-print(t1)
-print([elem.random.val for elem in t1])
-r1 = s.copyRandomList(t1)
-print(r1)
-print([elem.random.val for elem in r1])
+    for i, randomNext in enumerate(random):
+        lookup[i].random = lookup.get(randomNext)
 
-r2 = s.copyRandomList(t2)
-print(r2)
-print([elem.random.val if elem.random else None for elem in r2])
+    copy = s.copyRandomList(orig)
+    copy2 = s.copyRandomListSimpler(orig)
+
+    assert compareEqualAndCopied(orig, orig) == False
+    assert compareEqualAndCopied(copy, copy) == False
+    assert compareEqualAndCopied(orig, copy) == True
+    assert compareEqualAndCopied(orig, copy2) == True
+
+    # print(" order", " ->".join([f"{elem.val:2}" for elem in orig]))
+    # print("random", " ->".join([f"{rlookup[elem.random]:2}" if elem.random else " Ø" for elem in orig]))
