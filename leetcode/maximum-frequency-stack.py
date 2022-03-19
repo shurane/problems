9ef99554e06e99e3e36e72595536a09676df27c5
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-03-19 14:33:36 -0700

    removed unused code

--- leetcode/maximum-frequency-stack.py
+++ leetcode/maximum-frequency-stack.py
@@ -39,9 +39,6 @@ def pop(expected: int) -> Callable[[FreqStack], None]:
         assert retval == expected
     return f
 
-["FreqStack","push","push","push","push","push","push","pop","push","pop","push","pop","push","pop","push","pop","pop","pop","pop","pop","pop"]
-[[],[4],[0],[9],[3],[4],[2],[],[6],[],[1],[],[1],[],[4],[],[],[],[],[],[]]
-
 testcases = [[push(5), push(7), push(5), push(7), push(4), push(5), pop(5), pop(7), pop(5), pop(4), pop(7), pop(5)],
              [push(4), push(0), push(9), push(3), push(4), push(2), pop(4), push(6), pop(6), push(1), pop(1), push(1), pop(1), push(4), pop(4), pop(2), pop(3), pop(9), pop(0), pop(4)],
             ]
