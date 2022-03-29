de1e848a8b1a61aa997123990285191130eb34fb
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2022-03-29 15:22:13 -0700

    potd: slightly different solution, thanks to wareag1e. for dupe values, just increase lo+1, hi-1 (see 3sum with dupes)

--- leetcode/search-in-rotated-sorted-array-ii.py
+++ leetcode/search-in-rotated-sorted-array-ii.py
@@ -1,6 +1,8 @@
 from typing import List
 
 class Solution:
+    # similar to https://leetcode.com/problems/search-in-rotated-sorted-array-ii/discuss/28195/Python-easy-to-understand-solution-(with-comments).,
+    # also 44ms solution in the submission graph
     def search(self, nums: List[int], target: int) -> bool:
         lo = 0
         hi = len(nums) - 1
@@ -23,6 +25,32 @@ class Solution:
             else:
                 lo += 1
 
+        return False
+
+    # thank you wareag1e, readable solution
+    def search2(self, nums: List[int], target: int) -> bool:
+        lo = 0
+        hi = len(nums) - 1
+
+        while lo <= hi:
+            while lo < hi and nums[lo] == nums[lo+1]:
+                lo += 1
+            while lo < hi and nums[hi] == nums[hi-1]:
+                hi -= 1
+
+            mid = (lo + hi) // 2
+            if nums[mid] == target or nums[lo] == target or nums[hi] == target:
+                return True
+            elif nums[mid] >= nums[lo]:
+                if nums[lo] < target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            else:
+                if nums[mid] < target < nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
         return False
 
     def searchSlow(self, nums: List[int], target: int) -> bool:
@@ -80,6 +108,9 @@ s = Solution()
 assert s.search([2,5,6,0,0,1,2], 0) == True
 assert s.search([2,5,6,0,0,1,2], 3) == False
 assert s.search([1,0,1,1,1], 0) == True
+assert s.search2([2,5,6,0,0,1,2], 0) == True
+assert s.search2([2,5,6,0,0,1,2], 3) == False
+assert s.search2([1,0,1,1,1], 0) == True
 
 n = 32
 
@@ -87,6 +118,9 @@ l1 = [0 for _ in range(n)]
 assert s.search(l1, 0) == True
 assert s.search(l1, -1) == False
 assert s.search(l1, 1) == False
+assert s.search2(l1, 0) == True
+assert s.search2(l1, -1) == False
+assert s.search2(l1, 1) == False
 assert s.searchSlow(l1, 0) == True
 assert s.searchSlow(l1, -1) == False
 assert s.searchSlow(l1, 1) == False
@@ -99,11 +133,14 @@ for i in range(n*2):
 
     assert s.search(newl, -1) == False
     assert s.search(newl, n) == False
+    assert s.search2(newl, -1) == False
+    assert s.search2(newl, n) == False
     assert s.searchSlow(newl, -1) == False
     assert s.searchSlow(newl, n) == False
     for target in range(n):
         # print(target, newl)
         assert s.search(newl, target) == True
+        assert s.search2(newl, target) == True
         assert s.searchSlow(newl, target) == True
 
 l3 = [0]
@@ -113,21 +150,26 @@ for i in range(len(l3)):
     newl = l3[i:] + l3[:i]
     assert s.search(newl, -1) == False
     assert s.search(newl, n) == False
+    assert s.search2(newl, -1) == False
+    assert s.search2(newl, n) == False
     assert s.searchSlow(newl, -1) == False
     assert s.searchSlow(newl, n) == False
     for target in range(n):
         # print(target, newl)
         assert s.search(newl, target) == True
+        assert s.search2(newl, target) == True
         assert s.searchSlow(newl, target) == True
 
 for i in range(n):
     newl = [1] * n
     newl[i] = 0
     assert s.search(newl, 0) == True
+    assert s.search2(newl, 0) == True
     assert s.searchSlow(newl, 0) == True
 
 for i in range(n):
     newl = [0] * n
     newl[i] = 1
     assert s.search(newl, 1) == True
+    assert s.search2(newl, 1) == True
     assert s.searchSlow(newl, 1) == True
