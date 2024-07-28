a833b6cfe30c27a93ea86944688f724908ad603b
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2024-07-28 00:37:57 -0700

    fixed by short-circuiting when finding link from city 1 to city n

--- leetcode/cost-to-reach-city-with-discounts.cpp
+++ leetcode/cost-to-reach-city-with-discounts.cpp
@@ -2,6 +2,7 @@
 #include <queue>
 #include <tuple>
 #include <iostream>
+#include <iomanip>
 #include <functional>
 #include <climits>
 #include <assert.h>
@@ -10,7 +11,7 @@ using namespace std;
 
 class Solution {
 public:
-    int minimumCost(int n, vector<vector<int>>& highways, int discounts) {
+int minimumCost(int n, vector<vector<int>>& highways, int discounts) {
         vector<vector<pair<int, int>>> graph(n);
         int rate = 1;
 
@@ -32,35 +33,50 @@ public:
             citiesMinCosts[0][j] = 0;
         }
 
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> toVisit;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> toVisit;
         toVisit.push({0, 0, discounts});
 
-         while (!toVisit.empty()) {
-             auto [cost, city, discountLeft] = toVisit.top(); toVisit.pop();
-             //cout << "visiting: " << city << ", cost: " << cost << ", discountLeft: " << discountLeft << endl;
+        while (!toVisit.empty()) {
+            auto [cost, city, discountLeft] = toVisit.top(); toVisit.pop();
+            //cout << "visiting: " << city << ", cost: " << cost << ", discountLeft: " << discountLeft << endl;
+            if (city == n-1) return citiesMinCosts[n-1][discountLeft];
 
-             for (auto [next, toll]: graph[city]) {
-                 const int newCost = cost + toll;
-                 const int newCostDiscount = cost + toll / 2;
+            for (auto [next, toll]: graph[city]) {
+                const int newCost = cost + toll;
+                const int newCostDiscount = cost + toll / 2;
 
-                 if (newCost < citiesMinCosts[next][discountLeft]) {
-                     //cout << "no discount, found shorter distance from " << city << " to " << next << ": " << newCost << " < " << citiesMinCosts[next][discountLeft] << endl;
-                     citiesMinCosts[next][discountLeft] = newCost;
-                     toVisit.push({newCost, next, discountLeft});
-                 }
-                 if (discountLeft > 0 && newCostDiscount < citiesMinCosts[next][discountLeft-1]) {
-                     //cout << "   discount, found shorter distance from " << city << " to " << next << ": " << newCostDiscount << " < " << citiesMinCosts[next][discountLeft-1] << endl;
-                     citiesMinCosts[next][discountLeft - 1] = newCostDiscount;
-                     toVisit.push({newCostDiscount, next, discountLeft - 1});
-                 }
-             }
-         }
-         //cout << "value in citiesMinCosts[n-1][0]: " << citiesMinCosts[n-1][0] << endl;
-         if (citiesMinCosts[n-1][0] == INT_MAX) return -1;
+                if (newCost < citiesMinCosts[next][discountLeft]) {
+                    //cout << "no discount, found shorter distance from " << city << " to " << next << ": " << newCost << " < " << citiesMinCosts[next][discountLeft] << endl;
+                    citiesMinCosts[next][discountLeft] = newCost;
+                    toVisit.push({newCost, next, discountLeft});
+                }
+                if (discountLeft > 0 && newCostDiscount < citiesMinCosts[next][discountLeft-1]) {
+                    //cout << "   discount, found shorter distance from " << city << " to " << next << ": " << newCostDiscount << " < " << citiesMinCosts[next][discountLeft-1] << endl;
+                    citiesMinCosts[next][discountLeft - 1] = newCostDiscount;
+                    toVisit.push({newCostDiscount, next, discountLeft - 1});
+                }
+            }
+        }
+        // printMatrix(citiesMinCosts, n, discounts);
 
+        if (citiesMinCosts[n-1][0] == INT_MAX) return -1;
         return citiesMinCosts[n-1][0];
+    }
 
-        return 0;
+    void printMatrix(vector<vector<int>>& matrix, int n, int discounts) {
+        cout << "-- | ";
+        for (int j=0; j<discounts+1; j++) {
+            cout << setw(3) << j;
+        }
+        cout << endl;
+
+        for (int i=0; i<n; i++) {
+            cout << setw(2) << i << " | ";
+            for (int j=0; j<discounts+1; j++) {
+                cout << setw(3) << matrix[i][j];
+            }
+            cout << endl;
+         }
     }
 };
 
@@ -73,6 +89,7 @@ int main()
         {4, {{1,3,17}, {1,2,7}, {3,2,5}, {0,1,6}, {3,0,20}}, 20, 8},
         {4, {{0,1,3}, {2,3,2}}, 0, -1},
         {5, {{0,2,2},{2,1,4},{2,3,3},{3,1,4},{2,4,5},{3,0,5}}, 4, 3},
+        {6, {{3,5,55},{0,2,50},{2,4,7},{2,1,38},{3,0,47},{3,1,83},{4,3,10},{5,0,89},{4,1,11},{2,3,70},{5,2,89},{0,4,58},{0,1,45},{1,5,48},{4,5,72}}, 4, 44},
     };
 
     for (auto& [n, highways, discounts, expected]: testcases) {
@@ -80,4 +97,3 @@ int main()
     }
 
 }
-
