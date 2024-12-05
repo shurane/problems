da2d6992948601d7e636aa2aabd9f183a6be8159
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2024-12-05 11:33:08 -0800

    daily problem, faster solution

--- leetcode/move-pieces-to-obtain-a-string.cpp
+++ leetcode/move-pieces-to-obtain-a-string.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <utility>
 // #include <iostream>
 #include <assert.h>
 
@@ -8,71 +9,41 @@ using namespace std;
 class Solution {
 public:
     bool canChange(string start, string target) {
-        vector<int> l_positions;
-        vector<int> r_positions;
-        int l_count = 0;
-        int r_count = 0;
-        // const string initial = start;
+        vector<pair<int, char>> start_pos;
+        vector<pair<int, char>> target_pos;
 
-        for (int i=0; i<target.size(); i++) {
-            if (target[i] == 'L') {
-                l_positions.push_back(i);
-            } else if (target[i] == 'R') {
-                r_positions.push_back(i);
-            }
-            l_count += (start[i] == 'L');
-            r_count += (start[i] == 'R');
-        }
-
-        if (l_count != l_positions.size() || r_count != r_positions.size()) {
-            return false;
-        }
-
-        int matches = 0;
-        const int total = l_positions.size() + r_positions.size();
-        int j=0;
-        for (int i=0; i<start.size() && j < l_positions.size(); i++) {
+        for (int i=0; i<start.size(); i++) {
             if (start[i] == 'L') {
-                const int expected = l_positions[j];
-                int moveLeft = i;
-                while (expected <= moveLeft-1 && start[moveLeft-1] == '_') {
-                    moveLeft--;
-                }
+                start_pos.push_back({i, 'L'});
+            } else if (start[i] == 'R') {
+                start_pos.push_back({i, 'R'});
+            }
 
-                if (expected != moveLeft) {
-                    return false;
-                }
-
-                swap(start[i], start[moveLeft]);
-                matches++;
-                j++;
+            if (target[i] == 'L') {
+                target_pos.push_back({i, 'L'});
+            } else if (target[i] == 'R') {
+                target_pos.push_back({i, 'R'});
             }
         }
 
-        j=r_positions.size()-1;
-        for (int i=start.size()-1; i>=0 && j>=0; i--) {
-            if (start[i] == 'R') {
-                const int expected = r_positions[j];
-                int moveRight = i;
-                while (moveRight+1 <= expected && start[moveRight+1] == '_') {
-                    moveRight++;
-                }
-                if (expected != moveRight) {
-                    return false;
-                }
-
-                swap(start[i], start[moveRight]);
-                matches++;
-                j--;
+        int i=0;
+        int j=0;
+        for (;i<start_pos.size() && j<target_pos.size(); i++) {
+            if (start_pos[i].second != target_pos[j].second) {
+                return false;
             }
+
+            if (start_pos[i].second == 'L' && target_pos[j].first > start_pos[i].first) {
+                return false;
+            }
+
+            if (start_pos[i].second == 'R' && start_pos[i].first > target_pos[j].first) {
+                return false;
+            }
+            j++;
         }
 
-        // cout << "initial: " << initial << ", ";
-        // cout << "  final: " << start << ", expected: " << target << endl;
-        // cout << "l_count: " << l_count << ", l_pos.size(): " << l_positions.size() << ", ";
-        // cout << "r_count: " << r_count << ", r_pos.size(): " << r_positions.size() << ", ";
-        // cout << "matches: " << matches << ", total: " << total << endl;
-        return matches == total;
+        return i == start_pos.size() && j == target_pos.size();
     }
 };
 
@@ -93,4 +64,5 @@ int main() {
     assert(s.canChange("R_L_", "__LR") == false);
     assert(s.canChange("_R", "R_") == false);
     assert(s.canChange("R_L__R__R_", "_L______RR") == false);
+    assert(s.canChange("_L__R__R_L", "L______RR_") == false);
 }
