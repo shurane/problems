a45fc3e7626b55267e70ede47b7f9151b18f5c6e
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2025-02-11 11:24:48 -0800

    initial pass of a working version in python

---
+++ daily-coding-challenge/2025.02.11.with.ken.word.count.engine.py
@@ -0,0 +1,66 @@
+from string import ascii_lowercase
+from collections import defaultdict
+from pprint import pprint
+
+def lower_and_remove_punctuation(s: str):
+    return "".join(c for c in s.lower() if c in ascii_lowercase)
+
+def word_count_engine(document: str) -> list[list[str]]:
+    wordmap: dict[str, int] = defaultdict(int)
+    word_occurrence: dict[str, int] = dict()
+
+    for i, word in enumerate(document.split(" ")):
+        normalized_word = lower_and_remove_punctuation(word)
+        wordmap[normalized_word] += 1
+        if normalized_word not in word_occurrence:
+            word_occurrence[normalized_word] = i
+
+    if "" in wordmap:
+        del wordmap[""]
+
+    def keyfn(item: tuple[str, int]):
+        return (-item[1], word_occurrence[item[0]])
+
+    def to_str(item: tuple[str, int]):
+        return [item[0], str(item[1])]
+
+    values = list(map(to_str, sorted(wordmap.items(), key=keyfn)))
+    pprint(values)
+
+    return values
+
+s1 = "Practice makes perfect. you'll only get Perfect by practice. just practice!"
+s2 = 'Every book is a quotation; and every      house is a quotation out of all forests, and mines,  and stone quarries; and every man is a quotation from all his ancestors. '
+
+assert(word_count_engine(s1) == [
+    ['practice', '3'],
+    ['perfect', '2'],
+    ['makes', '1'],
+    ['youll', '1'],
+    ['only', '1'],
+    ['get', '1'],
+    ['by', '1'],
+    ['just', '1']
+])
+
+
+assert(word_count_engine(s2) == [
+    ['and', '4'],
+    ['every', '3'],
+    ['is', '3'],
+    ['a', '3'],
+    ['quotation', '3'],
+    ['all', '2'],
+    ['book', '1'],
+    ['house', '1'],
+    ['out', '1'],
+    ['of', '1'],
+    ['forests', '1'],
+    ['mines', '1'],
+    ['stone', '1'],
+    ['quarries', '1'],
+    ['man', '1'],
+    ['from', '1'],
+    ['his', '1'],
+    ['ancestors', '1']
+])
