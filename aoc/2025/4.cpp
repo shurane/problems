1a78adea7fcd09c3ca409c6fa3669a8fba3a2c61
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2025-12-26 23:36:02 -0800

    aoc day 4 part 2

--- aoc/2025/4.cpp
+++ aoc/2025/4.cpp
@@ -1,27 +1,26 @@
-#include <format>
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <queue>
 #include <array>
 #include <utility>
 #include <assert.h>
 
-#include "rang.hpp"
-
 using namespace std;
 
-bool reachable(const vector<string>& matrix, const int i, const int j, const int m, const int n) {
-    constexpr array<pair<int, int>, 8> directions = {{
-        { 0,  1},
-        { 0, -1},
-        { 1,  0},
-        {-1,  0},
-        { 1,  1},
-        { 1, -1},
-        {-1,  1},
-        {-1, -1},
-    }};
+constexpr array<pair<int, int>, 8> directions = {{
+    { 0,  1},
+    { 0, -1},
+    { 1,  0},
+    {-1,  0},
+    { 1,  1},
+    { 1, -1},
+    {-1,  1},
+    {-1, -1},
+}};
 
+bool reachable(const vector<string>& matrix, const int i, const int j, const int m, const int n) {
     int count = 0;
     for (const auto& [y, x]: directions) {
         const int ni = i + y;
@@ -56,13 +55,60 @@ void part1(istream&& file) {
         }
     }
 
-    for (const auto& line: matrix) {
-        cout << format("{}", line) << endl;
-    }
+    // for (const auto& line: matrix) {
+    //     cout << format("{}", line) << endl;
+    // }
     cout << "part 1: " << total << endl;
 }
+
+void part2(istream&& file) {
+    vector<string> matrix;
+
+    string line;
+
+    while(getline(file, line) && line.size() != 0) {
+        matrix.push_back(line);
+    }
+
+    const int m = matrix.size();
+    const int n = matrix[0].size();
+    queue<pair<int, int>> bfs;
+
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            if (matrix[i][j] == '@' && reachable(matrix, i, j, m, n)) {
+                matrix[i][j] = '.';
+                bfs.push({i, j});
+            }
+        }
+    }
+
+    int total = 0;
+    while (!bfs.empty()) {
+        auto [i, j] = bfs.front(); bfs.pop();
+        total++;
+
+        for (const auto& [y, x]: directions) {
+            int ni = i + y;
+            int nj = j + x;
+            if (0 <= ni && ni < m && 0 <= nj && nj < n && matrix[ni][nj] == '@' && reachable(matrix, ni, nj, m, n)) {
+                matrix[ni][nj] = '.';
+                bfs.push({ni, nj});
+            }
+        }
+
+    }
+
+    // for (const auto& line: matrix) {
+    //     cout << format("{}", line) << endl;
+    // }
+    cout << "part 2: " << total << endl;
+}
+
 
 int main() {
     part1(ifstream("4.sample.in"));
     part1(ifstream("4.in"));
+    part2(ifstream("4.sample.in"));
+    part2(ifstream("4.in"));
 }
